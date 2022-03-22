0929bab4e54356bbcabac29e39858b0f763b88e8
Jonatan Oliveira <jonatan.oliveira@opah.com.br>
2022-03-22 12:35:23 -0300

    Projeto criado

---
+++ src/test/java/br/com/jhow/cursoudemy/CursoudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhow.cursoudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
